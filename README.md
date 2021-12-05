394fdc7212e45d90feb22cfee1da10ef1dacff22
Jerry1811 <jerryhukpati@gmail.com>
2021-12-05 06:28:20 +0000

    make test data global and update README

--- README.md
+++ README.md
@@ -1,21 +1,23 @@
-GETTING STARTED
+## GETTING STARTED
 
-Clone or download patient management project from github.
-Repository URL: https://github.com/mPharma/qa-take-home-test.git
+Clone or download zip patient management project from github.
 
-Follow the READMe file to set up project
+Repository URL: `https://github.com/mPharma/qa-take-home-test.git`
 
-INSTALLATION AND RUNNING TEST PROJECT
+Follow the README file to set up project and run locally
+
+## INSTALLING AND RUNNING TEST PROJECT
 
 Download the project zip or clone the repository
 
-git clone https://github.com/Jerry1811/mpharma-selenium-test
+`git clone https://github.com/Jerry1811/mpharma-selenium-test`
 
+```
 cd mpharma-selenium-test
 pip install selenium behave
+```
 
-run project locally in the terminal at the project root directory with the command:
+run project locally in the terminal with the command:
 behave features/create_new_patient.feature or simply behave
 
 NB: Python has to be installed locally
-If project doesn't run, check that selenium and behave are added to project settings in pycharm ide
--- features/steps/create_new_patient.py
+++ features/steps/create_new_patient.py
@@ -2,31 +2,35 @@ import time
 from behave import *
 from selenium import webdriver
 
+url = "http://localhost:3000/"
+firstname = "john"
+middle_name = "henderson"
+lastname = "doe"
+phone_number = "0245876382"
+date_of_birth = "10/02/1991"
+user_address = "27 ama akroma street, Takoradi"
 
 @given('patient management system homepage is displayed')
 def step_impl(context):
     context.driver = webdriver.Chrome("resources/chromedriver.exe")
-    context.driver.get("http://localhost:3000/")
+    context.driver.get(url)
     context.driver.maximize_window()
 
 
 @when('the user enters their firstname')
 def enter_firstname(context):
-    firstname = "john"
     firstname_element = context.driver.find_element_by_css_selector('[data-test-id="first-name"]')
     firstname_element.send_keys(firstname)
 
 
 @when('enter their middle name')
 def enter_middle_name(context):
-    middle_name = "henderson"
     middle_name_element = context.driver.find_element_by_css_selector('[data-test-id="middle-name"]')
     middle_name_element.send_keys(middle_name)
 
 
 @when('enter their lastname')
 def enter_lastname(context):
-    lastname = "doe"
     lastname_element = context.driver.find_element_by_css_selector('[data-test-id="last-name"]')
     lastname_element.send_keys(lastname)
 
@@ -34,19 +38,19 @@ def enter_lastname(context):
 @when('enter phone number')
 def enter_phone_number(context):
     phone_element = context.driver.find_element_by_css_selector('[data-test-id="phone-number"]')
-    phone_element.send_keys("0245876382")
+    phone_element.send_keys(phone_number)
 
 
 @when('enter valid date of birth')
 def enter_dob(context):
     dob = context.driver.find_element_by_css_selector('[data-test-id="dob"]')
-    dob.send_keys("10/02/1991")
+    dob.send_keys(date_of_birth)
 
 
 @when('enter address')
 def enter_user_address(context):
     address = context.driver.find_element_by_css_selector('[data-test-id="address"]')
-    address.send_keys("egya amponsah avenue")
+    address.send_keys(user_address)
     time.sleep(2)
 
 
